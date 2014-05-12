71e9d658a35eb16702c7d1e6b4d786c30a1b2186
Tony Baron <anthony.t.baron@gamil.com>
2014-05-11 22:49:23 -0400

    and more files.  it almost seems like i don't do web dev.

--- static/templates/hundredPointChallenge.js
+++ static/templates/hundredPointChallenge.js
@@ -1,3 +1,15 @@
 /**
  * Created by tonybaron on 5/8/14.
  */
+define(['handlebars.runtime'], function(Handlebars) {
+
+    return Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+        this.compilerInfo = [4,'>= 1.0.0'];
+        helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+
+
+
+        return "<div class=\"row\">\n    <div class=\"col-md-12\">\n        <div class=\"row header\">Hundred Point Challenge</div>\n        <div class=\"leaderboard-container\">\n            <div class=\"leaderboard-header\">4-Week Challenge Leaderboard</div>\n            <div class=\"leaderboard-sub-header\">Points</div>\n            <div data-js=\"leaderboardItems\"></div>\n        </div>\n        <div class=\"leaderboard-footer\">View Full Leaderboard</div>\n        <div data-js=\"challengeContainer\" class=\"row challenge-container\">\n        </div>\n    </div>\n</div>\n";
+    })
+
+});
